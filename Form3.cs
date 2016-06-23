f76b2858be9567033674e3a869114d67c806a15a
hr <m0246973892@15qm-dea-3.xyz>
2016-06-24 01:40:31 +0900

    Create Form3.cs

---
+++ Form3.cs
@@ -0,0 +1,34 @@
+/* Developer page */
+
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace yamb
+{
+    public partial class Form3 : Form
+    {
+        public Form3()
+        {
+            InitializeComponent();
+        }
+
+        private void Form3_Load(object sender, EventArgs e)
+        {
+            
+        }
+
+        // Twitterを開く
+        private void linkLabel1_LinkClicked(object sender, LinkLabelLinkClickedEventArgs e)
+        {
+            Form4 form4 = new Form4();
+            form4.Show();
+        }
+    }
+}
