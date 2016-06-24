72477081a62a2ae07110307732f93aeb04103cdb
hr <m0246973892@15qm-dea-3.xyz>
2016-06-24 18:35:31 +0900

    Create Form5.cs

---
+++ Form5.cs
@@ -0,0 +1,27 @@
+/* Form3 Twitter View page */
+
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace yamb
+{
+    public partial class Form5 : Form
+    {
+        public Form5()
+        {
+            InitializeComponent();
+        }
+
+        private void Form5_Load(object sender, EventArgs e)
+        {
+            twiBrowser.Url = new Uri("https://twitter.com/YAOMIA1/");
+        }
+    }
+}
